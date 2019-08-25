1ce90a2288b87185e90409a316c46aeb2459381b
Sebastian Weber <sdw.post@waebers.de>
2019-08-25 18:19:18 +0200

    bump

--- lgamma.cpp
+++ lgamma.cpp
@@ -2,10 +2,15 @@
 
 #include <benchmark/benchmark.h>
 #include <boost/math/special_functions/gamma.hpp>
+#include <stan/math/prim/scal/fun/boost_policy.hpp>
 #include <cmath>
 
 
 const int N = 10000;
+
+const double offset = 1E30;
+//const double offset = 1E20;
+//const double offset = 0.5;
 
 using namespace boost::math::policies;
 typedef policy<promote_double<false> > fast_policy_t;
@@ -19,23 +24,40 @@ typedef boost::math::policies::policy<
     boost::math::policies::pole_error<boost::math::policies::errno_on_error>,
   boost::math::policies::promote_double<false> > boost_policy_t;
 
+typedef boost::math::policies::policy<
+    boost::math::policies::overflow_error<
+        boost::math::policies::errno_on_error>,
+    boost::math::policies::pole_error<boost::math::policies::errno_on_error>,
+  boost::math::policies::promote_double<true> > old_boost_policy_t;
+
+
+static void BM_old_boost(benchmark::State& state) {
+
+  for (auto _ : state) {
+    for (int i=0; i != N; i++) {
+      boost::math::lgamma(i + offset, old_boost_policy_t());
+    }
+  }
+}
+
+BENCHMARK(BM_old_boost);
 
 static void BM_boost(benchmark::State& state) {
 
   for (auto _ : state) {
     for (int i=0; i != N; i++) {
-      boost::math::lgamma(i +0.5, boost_policy_t());
+      boost::math::lgamma(i + offset, stan::math::boost_policy_t());
     }
   }
 }
 
-//BENCHMARK(BM_boost);
+BENCHMARK(BM_boost);
 
 static void BM_boost9(benchmark::State& state) {
 
   for (auto _ : state) {
     for (int i=0; i != N; i++) {
-      boost::math::lgamma(i +0.5, very_fast_policy_t());
+      boost::math::lgamma(i + offset, very_fast_policy_t());
     }
   }
   
@@ -45,8 +67,8 @@ static void BM_boost9(benchmark::State& state) {
 static void BM_std(benchmark::State& state) {
 
   for (auto _ : state) {
-    for (int i=0+0.5; i != N; i++) {
-      std::lgamma(i+0.5);
+    for (int i=0; i != N; i++) {
+      std::lgamma(i+offset);
     }
   }
   
@@ -58,8 +80,8 @@ static void BM_cstd(benchmark::State& state) {
   int sign = 0;
   
   for (auto _ : state) {
-    for (int i=0+0.5; i != N; i++) {
-      lgamma_r(i+0.5, &sign);
+    for (int i=0; i != N; i++) {
+      lgamma_r(i+offset, &sign);
     }
   }
   
